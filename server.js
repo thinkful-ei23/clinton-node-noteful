716a56c5d550c7463fb49648c3c855b26098dfbc
Clinton Owen <clint@clintonowen.com>
2018-07-30 14:32:50 -0500

    added server listening port

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 'use strict';
 
+const express = require('express');
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
+
+console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+  
+// });
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
