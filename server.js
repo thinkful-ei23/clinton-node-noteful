811823da2391c112b3c2906e4259e407c145bfe1
Clinton Owen <clint@clintonowen.com>
2018-08-03 13:44:41 -0500

    installed mocha, chai, chai-http

--- server.js
+++ server.js
@@ -40,8 +40,14 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+// Listen for incoming connections
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+// Export for testing
+module.exports = app;
